a645745e470163032dd6cae999b17809b668dfe1
RedBull3 <65456462+redbullweb3@users.noreply.github.com>
2022-10-30 23:03:50 +0000

    Update README.md

---
+++ MY PROJECTS/DefiTailwindApp/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+
+export default Navbar;
